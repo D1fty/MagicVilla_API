c49425dd526d381c641e348a05ca9026b71a5726
Damien DeCourcy <damien.decourcy@titanium.solutions>
2024-03-20 08:01:22 +1300

    Delete and Put

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,5 +1,4 @@
 ﻿using MagicVilla_VillaAPI.Data;
-using MagicVilla_VillaAPI.Models;
 using MagicVilla_VillaAPI.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,7 +15,7 @@ namespace MagicVilla_VillaAPI.Controllers
             return Ok(VillaStore.VillaList);
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name ="GetVilla")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -31,6 +30,78 @@ namespace MagicVilla_VillaAPI.Controllers
             return (villa == null)
                 ? NotFound()
                 : Ok(villa);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO villaDTO)
+        {
+            ActionResult<VillaDTO>? validation = ValidateVilla(villaDTO);
+            if (null != validation)
+                return validation;
+
+            villaDTO.Id = VillaStore.ClaimId();
+            VillaStore.VillaList.Add(villaDTO);
+
+            return CreatedAtRoute("GetVilla", new { villaDTO.Id }, villaDTO);
+        }
+
+        private ActionResult<VillaDTO>? ValidateVilla(VillaDTO villaDTO)
+        {
+            //if (!ModelState.IsValid)
+            //    return BadRequest(ModelState);
+            if (villaDTO == null)
+            {
+                return BadRequest(villaDTO);
+            }
+            else if (villaDTO.Id != 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            else if (VillaStore.VillaList.Any(v => v.Name == villaDTO.Name))
+            {
+                ModelState.AddModelError("VillaPreexistingError", "Villa already exists!");
+                return BadRequest(ModelState);
+            }
+
+            return null;
+        }
+
+        [HttpDelete("{id:int}", Name = "DeleteVilla")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult DeleteVilla(int id)
+        {
+            if (id == 0)
+                return BadRequest();
+            
+            var villa = VillaStore.VillaList.FirstOrDefault(v => v.Id == id);
+            if (villa == null)
+                return NotFound();
+
+            VillaStore.VillaList.Remove(villa);
+
+            return NoContent();
+        }
+
+        [HttpPut("{id:int}", Name = "UpdateVilla")]
+        public IActionResult UpdateVilla(int id, [FromBody]VillaDTO villaDTO)
+        {
+            if (villaDTO == null || id != villaDTO.Id)
+                return BadRequest(ModelState);
+
+            var villa = VillaStore.VillaList.FirstOrDefault(v => v.Id == id);
+            if (villa == null)
+                return NotFound();
+
+            villa.Name = villaDTO.Name;
+            villa.SqFt = villaDTO.SqFt;
+            villa.Occupancy = villaDTO.Occupancy;
+
+            return NoContent();
         }
     }
 }
--- MagicVilla_VillaAPI/Data/VillaStore.cs
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -4,10 +4,17 @@ namespace MagicVilla_VillaAPI.Data
 {
     public static class VillaStore
     {
+        private static int MaxId { get; set; } = 2;
+
         public static List<VillaDTO> VillaList = new List<VillaDTO>
         {
-            new VillaDTO { Id = 1, Name = "Pool View"},
-            new VillaDTO { Id = 2, Name = "Beach View"}
+            new VillaDTO { Id = 1, Name = "Pool View", SqFt = 100, Occupancy = 4},
+            new VillaDTO { Id = 2, Name = "Beach View", SqFt = 300, Occupancy = 3}
         };
+
+        public static int ClaimId()
+        {
+            return ++MaxId;
+        }
     }
 }
